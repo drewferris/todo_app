2c71a4c028465d82491c47b33fc4985fac2f7fe6
Drew Ferris <ddf8029@gmail.com>
2016-12-28 13:12:55 -0800

    finished writing the unit tests for TodoDataService

--- src/app/todo-data.service.spec.ts
+++ src/app/todo-data.service.spec.ts
@@ -28,7 +28,7 @@ describe('TodoDataService', () => {
       service.addTodo(todo2);
       expect(service.getAllTodos()).toEqual([todo1, todo2]);
     }));
-  })
+  });
 
   describe('#save(todo)', () => {
 
@@ -40,5 +40,49 @@ describe('TodoDataService', () => {
       expect(service.getTodoById(1)).toEqual(todo1);
       expect(service.getTodoById(2)).toEqual(todo2);
     }));
-  })
+
+    it('should not remove anything if todo with corresponding id is not found', inject([TodoDataService], (service: TodoDataService) => {
+      let todo1 = new Todo({title: 'Hello 1', complete: false});
+      let todo2 = new Todo({title: 'Hello 2', complete: true});
+      service.addTodo(todo1);
+      service.addTodo(todo2);
+      expect(service.getAllTodos()).toEqual([todo1, todo2]);
+      service.deleteTodoById(3);
+      expect(service.getAllTodos()).toEqual([todo1, todo2]);
+    }));
+  });
+
+  describe('#updateTodoById(id, value)', () => {
+
+    it('should return todo with the corresponding id and data', inject([TodoDataService], (service: TodoDataService) => {
+      let todo = new Todo({title: 'Hello 1', complete: false});
+      service.addTodo(todo);
+      let updatedTodo = service.updateTodoById(1, {
+        title: 'new title'
+      });
+      expect(updatedTodo.title).toEqual('new title');
+    }));
+
+    it('should return null if todo is not found', inject([TodoDataService], (service: TodoDataService) => {
+      let todo = new Todo({title: 'Hello 1', complete: false});
+      service.addTodo(todo);
+      let updatedTodo = service.updateTodoById(2, {
+        title: 'new title'
+      });
+      expect(updatedTodo).toEqual(null);
+    }));
+  });
+
+  describe('#toggleTodoComplete(todo)', () => {
+
+    it('should return the updated todo with the inverse complete status', inject([TodoDataService], (service: TodoDataService) => {
+      let todo = new Todo({title: 'Hello 1', complete: false});
+      service.addTodo(todo);
+      let updatedTodo = service.toggleTodoComplete(todo);
+      expect(updatedTodo.complete).toEqual(true);
+      service.toggleTodoComplete(todo);
+      expect(updatedTodo.complete).toEqual(false);
+    }));
+
+  });
 });
