e4adba86b74154fd01e2c9aee630435044c0f662
Drew Ferris <ddf8029@gmail.com>
2017-01-05 00:35:27 -0800

    updated package.json and wrote server

---
+++ server.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const todoRouter = require('./routes/todo-routes');
+
+const dbPort = process.env.MONGOLAB_URI || 'mongodb://localhost/dev_db';
+
+mongoose.connect(dbPort);
+
+app.use(express.static(_dirname + '/build'));
+
+app.use('/todo', todoRouter);
+
+app.use((req, res) => {
+    console.log('hit end');
+    res.status(404).json({message: 'route not found'});
+}).use((err, req, res, next) => {
+    res.status(500).json({message: err.message});
+    next(err);
+});
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('listening on' + (process.env.PORT || 3000));
+});
